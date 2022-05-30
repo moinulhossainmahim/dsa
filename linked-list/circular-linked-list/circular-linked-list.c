4e232c3a3f6bf13a4925486588271ec332ddcd4b
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-05-30 22:45:57 +0600

    added circular linked list creating and displaying methods

---
+++ linked-list/circular-linked-list/circular-linked-list.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+}*Head=NULL;
+
+void create(int A[], int n) {
+    int i;
+    struct Node *t,*last;
+    Head=(struct Node *)malloc(sizeof(struct Node));
+    Head->data=A[0];
+    Head->next=Head;
+    last=Head;
+
+    for(i=1; i<n; i++) {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=last->next;
+        last->next=t;
+        last=t;
+    }
+}
+
+void display(struct Node *h) {
+    do{
+        printf("%d ", h->data);
+        h=h->next;
+    }while(h!=Head);
+}
+
+void recursiveDisplay(struct Node *h) {
+    static int flag=0;
+    if(h!=Head || flag==0)
+    {
+        flag=1;
+        printf("%d ", h->data);
+        recursiveDisplay(h->next);
+    }
+    flag=0;
+}
+
+int main() {
+    int A[] = {2, 3, 4, 6, 7};
+    create(A, 5);
+    recursiveDisplay(Head);
+    return 0;
+}
