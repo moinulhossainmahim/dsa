0db474c74ea68a05bb8796e24303baf9be1f390e
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-06-27 22:25:48 +0600

    Added insert and delete method

--- ds/linked-list/doubly-circular-ll/doubly-circular-ll.c
+++ ds/linked-list/doubly-circular-ll/doubly-circular-ll.c
@@ -78,11 +78,110 @@ int countNode() {
   return len;
 }
 
+void insertStart() {
+  node *newNode;
+  newNode=createNode();
+  if(start==NULL) {
+    start=newNode;
+    newNode->prev=newNode->next=NULL;
+  } else {
+    newNode->prev=start->prev;
+    newNode->next=start;
+    start->prev->next=newNode;
+    start->prev=newNode;
+    start=newNode;
+  }
+}
+
+void insertEnd() {
+  node *newNode;
+  newNode=createNode();
+  if(start==NULL) {
+    start=newNode;
+    newNode->prev=newNode->next=NULL;
+  }
+  else {
+    newNode->prev=start->prev;
+    newNode->next=start;
+    start->prev->next=newNode;
+    start->prev=newNode;
+  }
+}
+
+void insertMid(int index) {
+  node *temp,*newNode;
+  int i;
+  temp=start;
+  if(index>1 && index<countNode()) {
+    newNode=createNode();
+    for(i=1; i<index-1; i++) {
+      temp=temp->next;
+    }
+    newNode->prev=temp;
+    newNode->next=temp->next;
+    temp->next->prev=newNode;
+    temp->next=newNode;
+  } else {
+    printf("Position %d is not a valid position\n", index);
+  }
+}
+
+void deleteStart() {
+  node *temp;
+  if(start==NULL)printf("There is no node to delete\n");
+  else {
+    if(countNode()==1) {
+      free(start);
+      start=NULL;
+    } else {
+      temp=start;
+      temp->prev->next=temp->next;
+      temp->next->prev=temp->prev;
+      start=start->next;
+      free(temp);
+    }
+  }
+}
+
+void deleteEnd() {
+  node *temp;
+  if(start==NULL)printf("There is no node to delete\n");
+  else {
+    if(countNode()==1) {
+      free(start);
+      start=NULL;
+    } else {
+      temp=start->prev;
+      temp->prev->next=start;
+      start->prev=temp->prev;
+      free(temp);
+    }
+    printf("Node has been deleted\n");
+  }
+}
+
+void deleteMid(int index) {
+  node *temp;
+  int i;
+  temp=start;
+  if(index>1 && index<countNode()) {
+    for(i=1; i<index; i++) {
+      temp=temp->next;
+    }
+    temp->prev->next=temp->next;
+    temp->next->prev=temp->prev;
+    free(temp);
+    printf("Node in position %d has been deleted\n", index);
+  } else {
+    printf("Position %d is not a valid position\n", index);
+  }
+}
+
 int main() {
   createList(5);
   display();
-  rightToLeftDisplay();
-  findLastNode();
+  deleteMid(2);
+  display();
   printf("Length of the list: %d\n", countNode());
   return 0;
 }
