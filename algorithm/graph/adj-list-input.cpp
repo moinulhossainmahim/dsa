020a0ffac96a77e8276eb35d50a32df313ff2f08
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-07-21 16:57:42 +0600

    Added weighted graph

--- algorithm/graph/adj-list-input.cpp
+++ algorithm/graph/adj-list-input.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 const int mx=1000;
 
-vector<int> adj[mx];
+vector<pair<int, int>> adj[mx];
 
 int main() {
   int n,e;
   cin >> n >> e;
 
   for(int i=1; i<=e; i++) {
-    int u,v;
-    cin >> u >> v;
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+    int u,v,w;
+    cin >> u >> v >> w;
+
+    adj[u].push_back({v, w});
+    adj[v].push_back({u, w});
   }
 
   for(int i=1; i<=n; i++) {
-    cout << "Adjacent list of node " << i << ": ";
+    cout << "Adjacent list of  " << i << ": ";
     for(auto u : adj[i]) {
-      cout << u << " ";
+      cout << "Node: "  << u.first << " And cost : " << u.second << " ";
     }
     cout << endl;
   }
